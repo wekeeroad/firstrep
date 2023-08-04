1ed4d83892f39629f3fb961e063661c9c769fb01
wekeeroad <129673723+wekeeroad@users.noreply.github.com>
2023-08-04 22:03:57 +0800

    Update pipelinetest.groovy

--- jenkinsfile/pipelinetest.groovy
+++ jenkinsfile/pipelinetest.groovy
@@ -5,6 +5,22 @@ pipeline {
       steps {
         echo 'Hello world'
       }
+      post {
+        always {
+          sh "echo 'stage Build finish!'"
+        }
+      }
     } 
+  }
+  post {
+    changed {
+      echo "pipeline post changed!"
+    }
+    always {
+      echo "pipeline post always!"
+    }
+    success {
+      echo "pipeline post success!"
+    }
   }
 }
