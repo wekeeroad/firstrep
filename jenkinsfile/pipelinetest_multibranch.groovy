d14798cdd0251d99be7e3ca74c5a5e682b2e8704
wekeeroad <129673723+wekeeroad@users.noreply.github.com>
2023-08-12 22:26:48 +0800

    Create pipelinetest_multibranch.groovy

---
+++ jenkinsfile/pipelinetest_multibranch.groovy
@@ -0,0 +1,30 @@
+pipeline {
+  agent any
+
+  options {
+    buildDiscarder(logRotator(numToKeepStr: '10', daysToKeepStr: '3'))
+    retry(3)
+    timeout(time: 10, unit: 'HOURS')
+  }
+
+  stages {
+    stage('environment_orig') {
+      steps {
+        echo "${env.BUILD_NUMBER}" 
+        echo "${env.BRANCH_NAME}"
+        echo "${env.BUILD_URL}"
+        echo "${env.GIT_BRANCH}"
+      }
+    }
+  post {
+    changed {
+      echo "pipeline post changed!"
+    }
+    always {
+      echo "pipeline post always!"
+    }
+    success {
+      echo "pipeline post success!"
+    }
+  }
+}
